d32d3d9c733e60900216406e7635a984dfdebf7f
Maria Heloisa <74155349+helofpizarro@users.noreply.github.com>
2025-07-26 23:26:12 +0000

    nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,18 +11,36 @@ int main() {
     float pc1, pc2;
     float sp1, sp2;
 
-       // Entrada de dados
+    printf("Digite o nome da primeira cidade: ");
     scanf(" %[^\n]", nome1);
+    prinf("Digite a populacao da primeira cidade: ");
+    scanf("%lu", &pop1);
+    printf("Digite a area da primeira cidade: ");
+    scanf("%f", &area1);
+    printf("Digite o pib da primeira cidade: ");
+    scanf("%f", pib1);
+    printf("Digite o numeros de pontos turisticos da primeira cidade: ");
+    scanf("%d", &pontos1);
     scanf("%lu %f %f %d", &pop1, &area1, &pib1, &pontos1);
 
+    
+    printf("Digite o nome da segunda cidade: ");
     scanf(" %[^\n]", nome2);
+    printf("Digite a populacao da segunda cidade: ");
+    scanf("%lu", &pop2);
+    printf("Digita a area da segunda cidade: ");
+    scanf("%f", &area2);
+    printf("Digite o pib da segunda cidade: ");
+    scanf("%f", pib2);
+    printf("Digite o numero de pontos turisticos da segunda cidade: ");
+    scanf(" %[^\n]", pontos2);
     scanf("%lu %f %f %d", &pop2, &area2, &pib2, &pontos2);
 
     // Calcula atributos derivados
     dens1 = pop1 / area1;
     dens2 = pop2 / area2;
-    pc1 = pib1 / pop1;
-    pc2 = pib2 / pop2;
+    pc1 = (pib1 * 1000000000.0f) / pop1;
+    pc2 = (pib2 * 1000000000.0f) / pop2;
 
     // Calcula super poder
     sp1 = pop1 + area1 + pib1 + pontos1 + pc1 + (1.0f / dens1);
